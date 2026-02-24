d50283ed0eb8b52309fc52b846f6cf4ba2d0042c
yuripbong <128915302+yuripbong@users.noreply.github.com>
2026-02-24 18:00:23 +0900

    yuri5: easy.py

---
+++ yuri/week5/easy.py
@@ -0,0 +1,10 @@
+def solution(x):
+    # f = [0, 1, 1, 2, 3, 5]
+    f = [0] * (x+1)
+    f[0] = 0
+    f[1] = 1
+    
+    for i in range(2, x+1):
+        f[i] = f[i-2] + f[i-1]
+    
+    return f[-1] % 1234567
